8de2b445e4afc4413730380c9f5860737e63efd6
Ali Yaman <ali4yaman@gmail.com>
2025-01-19 22:15:36 +0300

    commit atbash

---
+++ atbash/atbash.py
@@ -0,0 +1,12 @@
+from string import ascii_lowercase as letters
+letters_reverse = letters[::-1]
+encrypted = []
+
+def atbash(text):
+    text = text.replace(" ","")
+    for i in text:
+        if i in letters:
+            encrypted.append(letters_reverse[letters.index(i)])
+        else:
+            print(f"String {i} is passed because its not in ascii letters")
+    print(f"Encrypted/decrypted text is {"".join(encrypted)}")
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from vigenere.vigenere import vigenere
 from caesar.encrypt import caesar
 from caesar.bruteforce import brute
+from atbash.atbash import atbash
 #print(caesar(7,"ilovecryptography"))
 #print(brute("Uifsfjtbsfufstfpef"))
 
 #text key
-vigenere("","kca","d")
+#vigenere("","kca","d")
+
+atbash("zangıç_")
