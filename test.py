ba08d15deb336af451a571fb461786a6c9ab6583
Ali <64737576+Aliyaman@users.noreply.github.com>
2025-01-19 21:57:51 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from vigenere.vigenere import vigenere
-from caesar.encrypt import caesar
-from caesar.bruteforce import brute
-#print(caesar(7,"ilovecryptography"))
-#print(brute("Uifsfjtbsfufstfpef"))
-
-#text key
-vigenere("","kca","d")
